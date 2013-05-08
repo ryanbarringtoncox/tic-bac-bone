6d25a001b5700d0d9e10360168997a9807870b7f
core.editor <ryanbarrington@gmail.com>
2013-05-08 10:35:42 -0400

    first commit

---
+++ tic-tac-toe.js
@@ -0,0 +1,45 @@
+(function($) {
+  var GameView = Backbone.View.extend({
+    
+    el: $(".square-wrapper"),
+        
+    events: {
+      "click button#reset": "reset",
+      "click .square": "makeMove"
+    },
+    
+    initialize: function() {
+      _.bindAll(this, "render", "reset", "makeMove");
+      
+      this.render();
+    },
+    
+    render: function() {
+      //console.log("render called");
+      var self = this;
+      
+      //draw the squares
+      for (var i = 1; i <= 9; i++) {
+        $(this.el).append("<div class='square' id='square-" + i + "'></div>");
+      }
+      
+      //append reset button
+      $(this.el).append("<button id='reset'>Reset</button>")
+
+    },
+    
+    reset: function() {
+      console.log("reset called!");
+    },
+    
+    makeMove: function() {
+      console.log("makeMove called!");
+      console.log(this);
+    },
+    
+  });
+  
+  var gameView = new GameView();
+  
+})(jQuery); 
+
