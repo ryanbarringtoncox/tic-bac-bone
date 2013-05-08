db72f439643e871fc2b4ad37f06923aaf48600f5
core.editor <ryanbarrington@gmail.com>
2013-05-08 17:02:46 -0400

    adds clicks and reset functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,22 @@
 (function($) {
+  
+  var Game = Backbone.Model.extend({
+    defaults: {
+      player1: [],
+      player2: [],
+      update: function(currSquare) {
+        console.log(currSquare + " clicked");
+        var currDiv = "#" + currSquare;
+        
+        //check to see if it's already clicked
+        
+        //update the square
+        $(currDiv).addClass("clicked");
+        $(currDiv).append("<span>X</span>");
+      }      
+    }
+  });
+  
   var GameView = Backbone.View.extend({
     
     el: $(".square-wrapper"),
@@ -11,12 +29,16 @@
     initialize: function() {
       _.bindAll(this, "render", "reset", "makeMove");
       
+      this.game = new Game();
       this.render();
     },
     
     render: function() {
       //console.log("render called");
       var self = this;
+      
+      //clear the board
+      $(this.el).html("");
       
       //draw the squares
       for (var i = 1; i <= 9; i++) {
@@ -30,11 +52,12 @@
     
     reset: function() {
       console.log("reset called!");
+      this.initialize();
     },
     
-    makeMove: function() {
-      console.log("makeMove called!");
-      console.log(this);
+    makeMove: function(e) {
+      var currSquare = $(e.currentTarget).attr("id");
+      this.game.attributes.update(currSquare);
     },
     
   });
