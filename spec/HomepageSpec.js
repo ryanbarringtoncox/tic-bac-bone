5179b88f6a103e2888264c6fbce3a387017baa74
ryanbarringtoncox <ryanbarrington@gmail.com>
2013-05-09 09:35:32 -0400

    gets basic jasmine test working

--- spec/HomepageSpec.js
+++ spec/HomepageSpec.js
@@ -1,7 +1,33 @@
-console.log("boo");
+var Game = Backbone.Model.extend({
+  defaults: {
+    welcomeMessage: "Welcome to the game!",
+    player1: [],
+    player2: [],
+    
+    //call this "move", should just take int 1-9 and check that it's right
+    update: function(currSquare) {
+      console.log(currSquare + " clicked");
+      var currDiv = "#" + currSquare;
+      
+      //check to see if it's already clicked
+      
+      //update the square
+      $(currDiv).addClass("clicked");
+      $(currDiv).append("<span>X</span>");
+    }
+    
+    //isSquareAvailable(Number)
+    
+    //updatePlayer(Number)
+    
+    //isGameWon
+    
+  }
+});
 
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+describe("Game Model", function() {
+  it("should return welcome message", function() {
+    var game = new Game();
+    expect(game.get("welcomeMessage")).toEqual("Welcome to the game!");
   });
 });
